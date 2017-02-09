87a5453a045c33429bf24dd1702d5f500e20c275
kcmin <kcmin@uci.edu>
2017-02-08 16:17:24 -0800

    asf
    asf

--- src/components/contact.react.js
+++ src/components/contact.react.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 //const store = [{userName: Tony, userAddress: Klondike, userQuote: Food}];
 // this is a test pls ignore this
-// hey remember to merge your branch with master first
+// NEVER MERGE W/ MASTER FIRST. I WILL NEVER MERGE. (JOSH WILL)
+// ME, WORK ON MY BRANCH. GIT-PULL ORIGIN MASTER, THEN AFTER MERGED IN
 // and then send your pull request
 export default class Contact extends Component {
 
