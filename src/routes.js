c4d2cce2c8924d4e7d53ff586a19165c2c955e5d
jdalpert <josh1994bv@gmail.com>
2017-02-08 16:48:30 -0800

    added test file

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Route, IndexRoute, Link } from 'react-router';
 import App from './components/app.react';
 import Child from './components/child.react';
 import Display from './components/display.react';
+import Test from './components/test.react';
 
 //import Greeting from './components/greeting.react';
 
@@ -15,6 +16,6 @@ export default (
 		<IndexRoute component={Child}/>
 		<Route path="/display" component={Display} />
 		<Route path="/child" component={Child}/>
-		<Route path="/test" component={test}/>
+		<Route path="/test" component={Test}/>
 	</Route>
 );
