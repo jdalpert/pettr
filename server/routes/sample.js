ce0d9848c04a92b00c14035a1bd2389e5d9f8c5a
kcmin <kcmin@uci.edu>
2017-02-28 15:29:57 -0800

    schema fixed
    fixed teh schema stuff and added some calls

--- server/routes/sample.js
+++ server/routes/sample.js
@@ -55,8 +55,9 @@ router.get('/file/:id',function(req,res){
 
 router.post('/', function(req, res){
 	console.log(req.body);
-	new Info(req.body).save();
-	res.send(req.body);
+	new Info(req.body).save( function(err, user){
+        res.send(user._id);   
+    });
 })
 
 module.exports = router;
